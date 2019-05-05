c0f38a1549d74494258fab5ab1c344b15b6666f8
Michael Stacy <michael.stacy@wallawalla.edu>
2019-05-05 06:52:30 +0000

    main file

---
+++ mian.py
@@ -0,0 +1,12 @@
+#this is the balanced brackets coding challenge main function file
+#please see README.md for instructions
+
+#import necesarry libraries
+
+#define global variables
+
+#import other files
+
+#main function
+def main():
+    print("test")
